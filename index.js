7ff8c2e51fe4d98b87992db883ab2c038ad751e2
AdotHamilton <austin.hamilton.j@gmail.com>
2021-09-13 00:03:42 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        name: "Austin",
+        header1: true,
+        header2: false,
+        tech: [
+         "Vue.js",
+         "css animation",
+         "Firebase"
+        ],
+        financial: [
+            "Investing",
+            "Bugeting",
+            "Balancing a checkbook"
+        ]
+
+    }
+})
